3660f2bce31b994f446be51c061e434a8166ff10
Animesh <animesh.dey@weavers-web.com>
2022-08-16 15:43:36 +0530

    aligns setup

---
+++ src/Components/LoginPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="bg-[#F5E3E4] h-screen w-screen flex justify-center items-center">
+      <div className="w-2/4 h-2/4 flex flex-row">
+        <div className="w-1/2"></div>
+        <div className="w-1/2 bg-[#FF565F]"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
