71dcd9ec183db027c07bed0b0389b31131314fbc
Animesh <animesh.dey@weavers-web.com>
2022-08-16 15:57:35 +0530

    complete align and shadow and background color setup

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -3,9 +3,9 @@ import React from "react";
 const LoginPage = () => {
   return (
     <div className="bg-[#F5E3E4] h-screen w-screen flex justify-center items-center">
-      <div className="w-2/4 h-2/4 flex flex-row">
-        <div className="w-1/2"></div>
-        <div className="w-1/2 bg-[#FF565F]"></div>
+      <div className="w-[60%] h-[60%] flex flex-row rounded-lg shadow-xl">
+        <div className="w-1/2 bg-[#FFFEFF] rounded-tl-lg rounded-bl-lg"></div>
+        <div className="w-1/2 bg-[#FF565F] rounded-tr-lg rounded-br-lg"></div>
       </div>
     </div>
   );
